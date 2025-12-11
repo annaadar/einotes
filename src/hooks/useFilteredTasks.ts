40ea3a413ae7f6a9c23af9622e430f8cd3c5e5f8
annaadar <anna.adar270@gmail.com>
2025-12-11 13:36:22 +0200

    fix: remove unnecessary useEffect

--- src/hooks/useFilteredTasks.ts
+++ src/hooks/useFilteredTasks.ts
@@ -1,16 +1,15 @@
 import useDatesStore from "@store/DatesStore";
 import useTasksStore from "@store/TasksStore";
-import { useState, useEffect } from "react";
-import { Task } from "@typings/Task";
+import { useMemo } from "react";
 import { compareDates } from "utils/compareDates";
 
 export default function useFilteredTasks() {
-  const [filteredTasks, setFilteredTasks] = useState<Task[]>();
   const selectedDate = useDatesStore((state) => state.selectedDate);
   const allTasks = useTasksStore((state) => state.tasks);
-  useEffect(() => {
-    const tasks = allTasks.filter((task) => compareDates(task, selectedDate));
-    setFilteredTasks(tasks);
+
+  const filteredTasks = useMemo(() => {
+    return allTasks.filter((task) => compareDates(task, selectedDate));
   }, [selectedDate, allTasks]);
+
   return filteredTasks;
 }
