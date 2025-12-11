034394c71905ac4adcd2b3b36ce2ca80f786d825
annaadar <anna.adar270@gmail.com>
2025-12-11 13:38:09 +0200

    refactor: move greeting text element to a separate component

--- src/app/SigninScreen.tsx
+++ src/app/SigninScreen.tsx
@@ -1,45 +1,38 @@
-import { ActivityIndicator, Animated, Text, View } from "react-native";
-import { useContext, useEffect, useRef, useState } from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
-import useSigninAnimation from "@hooks/useSigninAnimation";
+import Greeting from "components/ui/Greeting";
 //TODO: remove signout button,  add loading state when auto-signin.
 export default function SigninScreen() {
   const { fbSignIn } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
-  const slideAnimation = useSigninAnimation()
+  const onSignin = async () => {
+    try {
+      setLoading(true);
+      await fbSignIn();
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1, gap: "5%" }}>
-      <Animated.View style={{ transform: [{ translateY: slideAnimation }] }}>
-        <Text
-          style={{
-            textAlign: "center",
-            fontWeight: "700",
-            fontSize: 38,
-            marginHorizontal:20,
-            color: "#611515ff",
-            textShadowColor: "#dcc2baff",
-            textShadowOffset: { width: 0, height: 3 },
-            textShadowRadius: 0.3
-          }}
-        >
-          Signin using Google to start using Einotes!
-        </Text>
-      </Animated.View>
+    <View style={styles.ScreenContainer}>
+      <Greeting></Greeting>
       <GoogleSigninButton
         size={GoogleSigninButton.Size.Wide}
         color={GoogleSigninButton.Color.Light}
-        onPress={async () => {
-          try {
-            setLoading(true);
-            await fbSignIn();
-          } finally {
-            setLoading(false);
-          }
-        }}
+        onPress={onSignin}
         disabled={loading}
       />
       {loading && <ActivityIndicator></ActivityIndicator>}
     </View>
   );
 }
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    gap: "5%",
+  },
+});
---
+++ src/components/ui/Greeting.tsx
@@ -0,0 +1,23 @@
+import useSigninAnimation from "@hooks/useSigninAnimation";
+import { Animated, StyleSheet, Text } from "react-native";
+
+export default function Greeting() {
+  const slideAnimation = useSigninAnimation();
+  return (
+    <Animated.View style={{ transform: [{ translateY: slideAnimation }] }}>
+      <Text style={styles.text}>Sign-in using Google to start using Einotes!</Text>
+    </Animated.View>
+  );
+}
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 38,
+    marginHorizontal: 20,
+    color: "#611515ff",
+    textShadowColor: "#dcc2baff",
+    textShadowOffset: { width: 0, height: 3 },
+    textShadowRadius: 0.3,
+  },
+});
